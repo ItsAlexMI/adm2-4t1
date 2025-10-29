e06ffb9f90c47ab2c91a288c170bb7ab3292ddd2
ItsAlexMI <alexmartinezirias2004@gmail.com>
2025-10-28 21:36:34 -0600

    🎉

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,12 +1,23 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 
+// @ts-ignore
+// eslint-disable-next-line
+declare global {
+  interface Window {
+    inkeepWidget?: any;
+    InkeepWidget?: any;
+  }
+}
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
+    <>
+      <DocsLayout tree={source.pageTree} {...baseOptions}>
+        {children}
+      </DocsLayout>
+    </>
   );
 }
